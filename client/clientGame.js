ac27f635a484bcf90a8a477a7d799ca83ebec250
nazif786 <nazif786@tamu.edu>
2016-03-27 13:09:18 -0500

    Update clientGame.js

--- client/clientGame.js
+++ client/clientGame.js
@@ -16,53 +16,55 @@ var timer
 **************************************************/
 
 // creating phaser game instance
-var game = new Phaser.Game(800, // width in pixels
-                          600,  // height in pixels
+var game = new Phaser.Game(800, // width of game screen in pixels
+			  600,  // height of game screen in pixels
                           Phaser.AUTO,
-                          { preload: preload, create: create, update: update, render: render }  // list of functions that define game state
+                          { preload, create, update, render }  // list of functions that define game state
 )
 
 // preload assets
 function preload () {
-  game.load.image('background', 'assets/backgound.png')
-  game.load.image('shark', 'assets/shark.png)
-  game.load.image('wreckage', 'assets/wreckage.png')
-  game.load.image('treasure', 'assets/treasure.png')
-  /* load other images as needed */
+ 	game.loadImage('background', 'assets/backgound.png')
+ 	game.loadImage('shark', 'assets/shark.png)
+ 	game.loadImage('wreckage', 'assets/wreckage.png')
+ 	game.loadImage('treasure', 'assets/treasure.png')
+ 	/* load other images as needed */
 }
+
+
 // create the game objects for play to begin
 function create () {
-  // Initialise socket connection
+  	// Initialise socket connection
 	socket = io.connect("http://localhost", {port: 20202, transports: ["websocket"]});
   
-  // list of active players connected to server
-  players = []
-  // starting position of our player
-  var startX = Math.random() mod 800
-  var startY = Math.random() mod 600
-  cursors = game.input.keyboard.createCursorKeys()
-  // Start listening for events
-  setEventHandlers()
+  	// list of active players connected to server
+  	players = []
+  	// starting position of our player
+  	var startX = Math.random() mod 800
+  	var startY = Math.random() mod 600
+  	cursors = game.input.keyboard.createCursorKeys()
+  	// Start listening for events
+  	setEventHandlers()
 }
 
 
 function update () {
-  for (var i = 0; i < enemies.length; i++) {
-    if (enemies[i].alive) {
-      enemies[i].update()
-      game.physics.collide(player, enemies[i].player)
-    }
-  }
-  if (cursors.left.isDown) {
-  } else if (cursors.right.isDown) {
-  }
-  if (cursors.up.isDown) {
-    // The speed we'll travel at
-  } else {
+  	for (var i = 0; i < enemies.length; i++) {
+    		if (enemies[i].alive) {
+      			enemies[i].update()
+      			game.physics.collide(player, enemies[i].player)
+    		}
+  	}
+  	if (cursors.left.isDown) {
+  	} else if (cursors.right.isDown) {
+  	}
+  	if (cursors.up.isDown) {
+	  // The speed we'll travel at
+  	} else {
 
-  }
+  	}
   
-  socket.emit('move player', { x: player.x, y: player.y })
+  	socket.emit('move player', { x: player.x, y: player.y })
 }
 
 
