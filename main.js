99f284e0a0559b111f240431dde9820275d75f3b
nazif786 <nazif786@tamu.edu>
2016-04-03 13:50:39 -0500

    Explodes at Divers Position

--- main.js
+++ main.js
@@ -9,31 +9,30 @@ var diver;
 var mainState = {
 	preload:function(){
 		game.load.image('background', "assets/background.png");
-		//game.load.image('torch', "assets/flashlight.png");
 		game.load.image('diver', 'assets/diver.jpg');
 		game.load.spritesheet('kaboom', 'assets/explosion.png', 64, 64);
 	},
 	
 	create:function(){
+		// adding backgound image
 		oceanfloor = game.add.tileSprite(0,0,800,600, 'background');
 		
+		// adding diver's movement
 		diver = game.add.sprite( game.world.centerX, game.world.centerY, 'diver');
 		diver.anchor.setTo(0.5, 0.5);
-		diver.scale.setTo(0.2, 0.2);
-		
+		diver.scale.setTo(0.15, 0.15);
 		game.physics.enable(diver, Phaser.Physics.ARCADE);
 		diver.body.drag.x = 100;
 		diver.body.drag.y = 100;
 		
-		//flashlight = game.add.sprite(game.world.centerX, game.world.centerY, 'torch');
-		//flashlight.anchor.setTo(0.5, 0.5);
-		
-		explosion = game.add.sprite(40, 100, 'kaboom');
+		// bomb explosion
+		explosion = game.add.sprite(diver.x, diver.y, 'kaboom');
 		explosion.animations.add('explode', [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24])
 	},
 	
 	update:function(){
 		if (game.input.keyboard.isDown(Phaser.Keyboard.B)){
+			explosion.reset(diver.x-200, diver.y+200);
 			explosion.animations.play('explode', 40, false);
 		}
 		if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
