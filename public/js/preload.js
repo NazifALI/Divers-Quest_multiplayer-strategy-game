804f770da29a02107799a946bee999651501ecad
nazif786 <nazif786@tamu.edu>
2016-04-24 16:00:15 -0500

    Update preload.js

--- public/js/preload.js
+++ public/js/preload.js
@@ -5,9 +5,9 @@ preload.prototype = {
         var loadingBar = this.add.sprite(game.world.centerX, game.world.centerY, "loading");
         loadingBar.anchor.setTo(0.5,0.5);
         this.load.setPreloadSprite(loadingBar);
-		this.game.load.image("gametitle","assets/logo.png");
-		this.game.load.image("play","assets/play3.png");
-		this.game.load.image("gameover","assets/gameover.png");
+	this.game.load.image("gametitle","assets/logo.png");
+	this.game.load.image("play","assets/play3.png");
+	this.game.load.image("gameover","assets/gameover.png");
         this.game.load.image('see', 'assets/sea.jpg')
         this.game.load.image('earth2', 'assets/dark_grass.png')
         this.game.load.image('back','assets/back.png')
@@ -15,15 +15,15 @@ preload.prototype = {
         this.game.load.image('shark', 'assets/shark.png')
         this.game.load.image('oxygen', 'assets/oxygen.png')
         this.game.load.image('wreckage', 'assets/wreckage.png')
-		this.game.load.image('torpedo', 'assets/torpedo.png');
-		this.game.load.image('enemy', 'assets/dude.png');
-		this.game.load.image('rock', 'assets/Rock.png');
-		this.game.load.image('rock2', 'assets/Rock2.png');
-		this.game.load.image('instruction', 'assets/button3.png');
-		this.game.load.image('arrow', 'assets/arrow.png');
-		this.game.load.image('backButton', 'assets/backButton.png');
-		this.game.load.image('leftmouse','assets/leftmouse.png')
-		this.game.load.image('spacebar','assets/spacebar.png')
+	this.game.load.image('torpedo', 'assets/torpedo.png');
+	this.game.load.image('enemy', 'assets/dude.png');
+	this.game.load.image('rock', 'assets/Rock.png');
+	this.game.load.image('rock2', 'assets/Rock2.png');
+	this.game.load.image('instruction', 'assets/button3.png');
+	this.game.load.image('arrow', 'assets/arrow.png');
+	this.game.load.image('backButton', 'assets/backButton.png');
+	this.game.load.image('leftmouse','assets/leftmouse.png')
+	this.game.load.image('spacebar','assets/spacebar.png')
         this.game.load.spritesheet('dude', 'assets/diversprite.png', 256, 256);
         this.game.load.spritesheet('kaboom', 'assets/explosion.png', 64, 64);
         this.game.load.audio('song', ['assets/audio/ComeAndFindMeBMix.mp3', 'assets/audio/ComeAndFindMeBMix.ogg']);
